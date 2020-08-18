b59ffc556e0042f188ce09486b967b29ffcf9974
Esa Halmkrona <ehalmkro@student.hive.fi>
2020-08-18 18:58:04 +0300

    index to work as a router

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
 <?php
-
+    session_start();
     require_once $_SERVER['DOCUMENT_ROOT'] . '/src/controllers/userController.php';
-    $controller = new userController();
-    $controller->handleRequest();
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/src/controllers/imageController.php';
+
+    $request = explode('/', trim($_SERVER['PATH_INFO'], '/'));
+    $controller = $request[0];
+    $method = $request[1];
+    $argument = $request[2];
+
+    switch ($controller) {
+        case 'userController': {
+            $ctrl = new userController();
+            $ctrl->$method();
+            break;
+        }
+        case 'imageController': {
+            $ctrl = new imageController();
+            $ctrl->$method();
+            break;
+        }
+        default:
+            if (isset($_SESSION['uid']))
+                include $_SERVER['DOCUMENT_ROOT'] . '/src/views/user_profile.php';
+            else
+                include $_SERVER['DOCUMENT_ROOT'] . '/src/views/homepage.php';
+    }
 
